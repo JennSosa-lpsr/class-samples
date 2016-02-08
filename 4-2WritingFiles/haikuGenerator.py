b026042ca4ae981d3c28211053f416228f24ec45
JennSosa-lpsr <rc.sosa.jennifer@gmail.com>
2016-02-08 21:33:10 +0000

    Finished program.

--- 4-2WritingFiles/haikuGenerator.py
+++ 4-2WritingFiles/haikuGenerator.py
@@ -1,4 +1,4 @@
-
+# We ask the user for inputs and convert their response into a string.
 print("Welcome to the Haiku Generator!")
 print("Provide the first line of your haiku.")
 x = str(raw_input())
@@ -7,8 +7,12 @@ y = str(raw_input())
 print("Provide the third line to your haiku.")
 z = str(raw_input())
 print("What file would you like to write your haiku into.")
+# We assing a variable to whatever they named their file.
 myFile = str(raw_input())
 print("Done! To view your haiku, type 'cat' and the name of your file at the command line")
+# Here we open the file to what they have named it.
 my_File = open((myFile), "a")
+# We put their haiku together on seperate lines and save their input on that file.
 my_File.write( x + '\n' + y + '\n' + z + '\n')
+# We closde the file
 my_File.close()
